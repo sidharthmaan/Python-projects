4ce58369c85aa2929b7b88936d67a1f9aefbdf5a
Sidharth <ssidharth984@gmail.com>
2024-01-18 13:05:57 +0530

    new file:   Fizz Buss problem.py 	new file:   Guess a number by computer .py 	new file:   Heads&Tails_Using_random no.py 	new file:   Madlibs.py

---
+++ Heads&Tails_Using_random no.py
@@ -0,0 +1,7 @@
+import random
+ramdom_Number = random.randint(1,2)
+if ramdom_Number == 1:
+    print("Heads")
+else:
+    print("Tails")
+# print(ramdom_Number)
---
+++ Madlibs.py
@@ -0,0 +1,10 @@
+enjoyment = input("Enjoyment when you start coding:")
+programming = input("Enter the language which you feel excited: ")
+mood = input("Enter about your mood while doing programming: ")
+slogan = input("Enter slogan with full energy!: ")
+
+madlibs = f"Computer programming is so {enjoyment}! It makes me so excited all the time \
+because I love to do {programming}. Stay {mood} and hydrated so that you can do what ever you want!\
+{slogan}."
+
+print(madlibs)
